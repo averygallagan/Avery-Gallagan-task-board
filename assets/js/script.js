de697fa95c9653df2a9e099e0d3d1cf6b4a5a021
Avery <averygallagan@yahoo.com>
2024-03-25 13:36:11 -0400

    modal added and generatetaskid function completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,13 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
+    return nextId++
 
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskCard = 
 
 }
 
@@ -34,5 +36,8 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    renderTaskList();
+
+    
 
 });
